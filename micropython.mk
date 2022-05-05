fa91283d7914b3e68f9ac3834b8d556698c4331b
MATTYGILO <mattygilo1@gmail.com>
2022-05-05 14:30:35 +0100

    Removed the c file

--- micropython.mk
+++ micropython.mk
@@ -3,7 +3,6 @@ EXAMPLE_MOD_DIR := $(USERMOD_DIR)
 # Add all C files to SRC_USERMOD.
 SRC_USERMOD += $(EXAMPLE_MOD_DIR)/microecc.c
 SRC_USERMOD += $(EXAMPLE_MOD_DIR)/ecc.c
-SRC_USERMOD += $(EXAMPLE_MOD_DIR)/bmp180.c
 SRC_USERMOD += $(EXAMPLE_MOD_DIR)/uECC.c
 
 # We can add our module folder to include paths if needed
